20b9f7cd7c29bd0e4c7e162529ccdae1dd08a317
Victor Gustafsson <kerrik.vg@gmail.com>
2024-07-05 22:48:54 +0200

    PArt two, steps 3-5

--- wrong.js
+++ wrong.js
@@ -4,3 +4,14 @@ logoText[0].style.color = "#384241";
 
 //2. 
 document.body.children[0].style.justifyContent = "flex-start";
+
+//3.
+document.body.children[0].style.borderBottomColor = "lightgray";
+
+//4.
+const recipeNameText = document.getElementById("recipe-name");
+recipeNameText.textContent = "Frozen Cheescake";
+
+//5.
+const timeElements = document.getElementsByClassName("time-container");
+timeElements[0].children[0].classList.add("material-icons");
