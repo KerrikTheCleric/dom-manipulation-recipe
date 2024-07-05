c7ffb736fd750394ab0af975e98367f8e6f17d3e
Victor Gustafsson <kerrik.vg@gmail.com>
2024-07-05 15:08:37 +0200

    Part one steps 4-6

--- index.js
+++ index.js
@@ -9,3 +9,23 @@ console.log(recipeNameElement.outerHTML);
 const descriptionElement = document.querySelector(".description");
 const descriptionStyle = window.getComputedStyle(descriptionElement);
 console.log(descriptionStyle.fontSize);
+
+//4. 
+const imageElement = document.querySelector(".image-container");
+console.log(imageElement.children[0].alt);
+
+//5.
+
+const imageItem = {url: string = "", height: number=0, width: number=0};
+imageItem.url = imageElement.children[0].src;
+imageItem.height = imageElement.children[0].naturalHeight;
+imageItem.width = imageElement.children[0].naturalWidth;
+
+console.log(imageItem);
+
+//6. 
+
+const ingredientListElement = document.querySelector(".ingredients-list-paste");
+
+
+console.log(ingredientListElement.children.length);
